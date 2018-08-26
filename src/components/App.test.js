b51c51bb2660a14219d9e1e3810b437084238c4f
Marcin Rudzki <mar.rudzki@gmail.com>
2018-08-26 16:58:46 +0200

    Add enzyme and 2nd test for the App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('App renders without a problem', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('App tests', () => {
+
+    it('renders without a problem', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('has text Hello World', () => {
+        const wrapper = shallow(<App/>);
+        //console.log(wrapper.debug());
+        expect(wrapper.find('i').text()).toBe('Hello World');
+    })
+
 })
