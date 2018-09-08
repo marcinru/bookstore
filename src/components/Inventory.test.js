20e45dae0d8a9c55409e3ece91e137d1b7e991cf
Marcin Rudzki <mar.rudzki@gmail.com>
2018-09-08 18:32:08 +0200

    Add Inventory component and test

---
+++ src/components/Inventory.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Inventory from './Inventory';
+
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Inventory tests', () => {
+
+    it('renders without a problem', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Inventory/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('has text Header', () => {
+        const wrapper = shallow(<Inventory/>);
+        expect(wrapper.text()).toBe('Inventory');
+    })
+
+})
